07f789a20620e65c71a9e305aa8c0553d09936b8
rebrow6 <junkmail@silenttrace.com>
2022-05-19 17:10:43 +0000

    Added slippery snow and fixed horizonatl movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,18 +2,22 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    [Range(0.0f, 5.0f)]
-    [SerializeField] float speed = 1.0f;
+    [Range(0.0f, 10.0f)]
+    [SerializeField] float speed = 5.0f;
 
-    [Range(75.0f, 300.0f)]
-    [SerializeField] float jumpForce = 100.0f;
+    [Range(75.0f, 1000.0f)]
+    [SerializeField] float jumpForce = 400.0f;
 
     private void Update()
     {
         float horizontal = Input.GetAxis("Horizontal") * speed;
         var rigidbody2D = GetComponent<Rigidbody2D>();
-        // we don't want to change y velocity outside of gravity or jump so leave it as it was.
-        rigidbody2D.velocity = new Vector2(horizontal, rigidbody2D.velocity.y);
+
+        if (Mathf.Abs(horizontal) >= 1 )
+        {
+            // we don't want to change y velocity outside of gravity or jump so leave it as it was.
+            rigidbody2D.velocity = new Vector2(horizontal, rigidbody2D.velocity.y);
+        }
 
         var animator = GetComponent<Animator>();
         bool walking = horizontal != 0;
